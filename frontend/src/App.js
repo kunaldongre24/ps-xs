d030d3f3dc2e79bddca5cd56342b7582b6437e33
kunaldongre24 <dongrekunal16@gmail.com>
2021-05-16 15:40:34 +0530

    added the strip to the top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ function App() {
 
   return (
     <div className="App">
+      <div className="strip"></div>
       <div class="modal">
         <div class="modal-body">
           <span className="close" onClick={closeModal}>
